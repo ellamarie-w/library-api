f2cd2863eb29ddf3e7890530ff0d743b0afb29d3
Grace Williams <108375656+ellamarie-w@users.noreply.github.com>
2022-12-12 14:31:18 -0600

    Add files via upload

---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Api\Http\Controllers\BookController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/books',[BookController::class,'index']); //mostramos los datos
+Route::post('/books/',[BookController::class,'store']); //creamos un nuevo articulo
+Route::put('/books/{id}',[BookController::class,'update']); //Actualizamos el articulo
+Route::delete('/books/{id}',[BookController::class,'destroy']); //Eliminamos el articulo 
