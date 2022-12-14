7bc714d9aa06bfb37ae0adb8608c4d55099f3f77
Grace Williams <108375656+ellamarie-w@users.noreply.github.com>
2022-12-14 15:37:43 -0600

    Add files via upload

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Api\Http\Controllers\BookController;
+use Api\Http\Controllers\UserIdController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,7 +20,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/books',[BookController::class,'index']); //mostramos los datos
-Route::post('/books/',[BookController::class,'store']); //creamos un nuevo articulo
-Route::put('/books/{id}',[BookController::class,'update']); //Actualizamos el articulo
-Route::delete('/books/{id}',[BookController::class,'destroy']); //Eliminamos el articulo 
+Route::get('/books',[BookController::class,'index']); //shows data
+Route::post('/books/',[BookController::class,'store']); //creates a new book
+Route::put('/books/{id}',[BookController::class,'update']); //updates book
+Route::delete('/books/{id}',[BookController::class,'destroy']); // deletes book 
+
+Route::get('/users',[UserIdController::class,'index']); //shows data
+Route::post('/users/',[UserIdController::class,'store']); //creates a new user
+Route::put('/users/{id}',[UserIdController::class,'update']); //updates user
+Route::delete('/users/{id}',[UserIdController::class,'destroy']); // deletes user
