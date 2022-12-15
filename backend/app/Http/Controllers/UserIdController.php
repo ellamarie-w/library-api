beecddbc7c62c49db099e3f071b26147ba28b010
Grace Williams <williamsciela@gmail.com>
2022-12-15 00:59:22 -0600

    Update UserIdController.php

--- backend/app/Http/Controllers/UserIdController.php
+++ backend/app/Http/Controllers/UserIdController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\UserId;
 
-class UserNameController extends Controller
+class UserIdController extends Controller
 {
     /**
      * Display a listing of the resource.
